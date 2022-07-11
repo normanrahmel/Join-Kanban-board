bb6b296ffae3d7e8554a22b9f2d04904bde540ba
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-07-11 17:39:23 +0200

    links fixed

--- smallest_backend_ever/mini_backend.js
+++ smallest_backend_ever/mini_backend.js
@@ -1,5 +1,5 @@
 let jsonFromServer = {};
-let BASE_SERVER_URL = 'http://gruppe-264.developerakademie.net/smallest_backend_ever';
+let BASE_SERVER_URL = '/projects/Join-Kanban-board/smallest_backend_ever';
 
 const backend = {
     setItem: function(key, item) {
